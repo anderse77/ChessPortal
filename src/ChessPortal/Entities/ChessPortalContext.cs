7955b229c1a8f621cbc36b078806d2e9f41d8ecc
DESKTOP-N6UL53L\Anders <anderse77@gmail.com>
2017-03-12 18:31:24 +0100

    Added Constructor to Context.

--- src/ChessPortal/Entities/ChessPortalContext.cs
+++ src/ChessPortal/Entities/ChessPortalContext.cs
@@ -10,6 +10,12 @@ namespace ChessPortal.Entities
 {
     public class ChessPortalContext : IdentityDbContext<ChessPlayer>
     {
+        public ChessPortalContext(DbContextOptions<ChessPortalContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
         public DbSet<ChallengeEntity> Challenges { get; set; }
         public DbSet<ChallengeAcceptEntity> AcceptedChallenges { get; set; }
         public DbSet<MoveEntity> Moves { get; set; }
